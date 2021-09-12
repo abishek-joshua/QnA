0b0adfab9f7a8d388ef298b6fb272794cdcd6869
abishek-joshua <abishekjoshua2001@gmail.com>
2021-09-12 19:12:41 +0530

    Added Image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import Logo from './Components/Logo';
 
 function App() {
   return (
     <div className="App">
       <h1>QnA</h1>
+      <Logo />
     </div>
   );
+    
 }
 
 export default App;
---
+++ src/Components/Logo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Logo = () => {
+    return(
+        <img  alt="Logo" src="https://cdn4.vectorstock.com/i/1000x1000/43/13/graphic-cartoon-character-qna-vector-35424313.jpg" width='300px' height='300px' />
+    );
+}
+
+export default Logo;

90ba5c317cbef282bab8488eae99a8d620e50afc
sathya2931 <sathyamoorthi2k01@gmail.com>
2021-09-20 19:51:26 +0530

    added components

---
+++ src/Components/Question/Question.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Question.css';
+class Question extends React.Component{
+  render(){
+    return(
+      <div className="question-container">
+        <p className="question-statement">{this.props.questionStatement}</p>
+        <p className="answers-and-time">5 answers . 10 hours ago</p>
+      </div>
+    );
+  }
+}
+
+export default Question;

65a9985b83702ecaecd1e221b0b6406599accc7d
abishek-joshua <abishekjoshua2001@gmail.com>
2021-09-23 21:17:08 +0530

    CSS and Register & forgot components added

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import SignIn from './Components/SignIn/SignIn';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  <SignIn />
   </React.StrictMode>,
   document.getElementById('root')
 );

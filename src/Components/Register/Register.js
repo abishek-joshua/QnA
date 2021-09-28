68a3394a5f04226c4228246e7b6d87ea2774e267
abishek-joshua <abishekjoshua2001@gmail.com>
2021-09-28 20:34:38 +0530

    Changes to CSS made

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -4,15 +4,14 @@ import './Register.css';
 const Register = () => {
     return (
         <div className="main">
-            <div className="Register">
+            <div className="register">
                 <h1>Register</h1>
-                <div className="formDetails">
+                <div className="form-details">
                     <strong>Email</strong> <input className="email-box" type="email" placeholder="xyz@student.tce.edu"></input>
                     <strong>Password</strong> <input className="password-box" type="password"></input>
+                    <strong> Confirm Password</strong> <input className="confirm-password-box" type="password"></input>
                     <button className="register-button">Register</button>
-                    <div className="sign-in-main">
-                        <p className="sign-in">Already have an account?</p>
-                    </div>
+                    <p className="sign-in">Already have an account?</p>
                 </div>
             </div>
         </div>

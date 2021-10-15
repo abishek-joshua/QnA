7aed36f09585034aaa391622abf25de0af6395c6
sathya2931 <sathyamoorthi2k01@gmail.com>
2021-10-15 22:25:22 +0530

    Nav Bar and some minor corrections

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -2,23 +2,39 @@ import React from "react";
 import "./Navigation.css";
 import { Link } from 'react-router-dom';
 import logout from "../../images/logout.svg";
-import menu from "../../images/menu.svg";
 import ask from "../../images/ask.svg";
-import home from "../../images/home.svg";
+import profile from "../../images/profile.svg";
 
 class Navigation extends React.Component {
     render() {
         return (
-            <>
-                <div className="sticky-nav">
-                    <button><img src={menu} alt="menu" /></button>
-                    <nav>
-                        <Link to='/'><img alt="home" src={home} /></Link>
-                        <Link to='/ask'> <img alt="ask" src={ask} /></Link>
-                        <Link to='/signin'><img alt="logout" src={logout} /></Link>
-                    </nav>
+            <nav className="sticky-nav">
+                <Link to='/'>
+                    <div className="app-title">QnA</div>
+                </Link>
+                <div className="nav-right">
+                    <Link to='/ask'>
+                        <div className="icon-button-desc">
+                            <img className="icon-button" alt="ask" src={ask} />
+                            Ask
+                        </div>
+                    </Link>
+
+                    <Link to='/signin'>
+                        <div className="icon-button-desc">
+                            <img className="icon-button" alt="logout" src={logout} />
+                            Logout
+                        </div>
+                    </Link>
+
+                    <Link to='/profile/:id'>
+                        <div className="icon-button-desc">
+                            <img className="icon-button" alt="profile" src={profile} />
+                            sathya
+                        </div>
+                    </Link>
                 </div>
-            </>
+            </nav>
         );
     }
 }
